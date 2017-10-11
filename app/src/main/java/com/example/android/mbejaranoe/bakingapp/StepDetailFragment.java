4c19c54609fe4eac15d55a6ae7080e6a70c55226
Manolo <mbejaranoe@gmail.com>
2017-10-11 11:46:04 +0200

    Populate ImageView (later on it will replaced with a video player) and description textview

--- app/src/main/java/com/example/android/mbejaranoe/bakingapp/StepDetailFragment.java
+++ app/src/main/java/com/example/android/mbejaranoe/bakingapp/StepDetailFragment.java
@@ -1,11 +1,16 @@
 package com.example.android.mbejaranoe.bakingapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 /**
  * Created by Manolo on 10/10/2017.
@@ -15,6 +20,9 @@ import android.view.ViewGroup;
 public class StepDetailFragment extends Fragment {
 
     private final String LOG_TAG = "StepDetailFragment";
+
+    private ImageView stepDetailThumbnailImageView;
+    private TextView stepDetailDescriptionTextView;
 
     // Constructor
     public StepDetailFragment() {
@@ -31,6 +39,30 @@ public class StepDetailFragment extends Fragment {
 
         // Inflate the step detail fragment layout
         View rootView = inflater.inflate(R.layout.fragment_step_detail, container, false);
+
+        stepDetailThumbnailImageView = (ImageView) rootView.findViewById(R.id.step_detail_thumbnail_image_view);
+        stepDetailDescriptionTextView = (TextView) rootView.findViewById(R.id.step_detail_description_text_view);
+
+        Intent intent = getActivity().getIntent();
+        if (intent.hasExtra("description")){
+            stepDetailDescriptionTextView.setText(intent.getStringExtra("description"));
+        } else {
+            stepDetailDescriptionTextView.setText(getResources().getString(R.string.no_step_description_message));
+        }
+
+        if (intent.hasExtra("thumbnailURL")) {
+            if (intent.getStringExtra("thumbnailURL").length() == 0) {
+                Picasso.with(getContext()).load(R.drawable.recipestepplaceholder_blue).into(stepDetailThumbnailImageView);
+            } else {
+                Picasso.with(getContext()).load(intent.getStringExtra("thumbnailURL")).placeholder(R.drawable.recipestepplaceholder_blue)
+                        .into(stepDetailThumbnailImageView);
+            }
+        }
+
+        /*
+        intent.putExtra("shortDescription", mSteps[stepIndex].getShortDescription());
+        intent.putExtra("videoURL", mSteps[stepIndex].getVideoURL());
+         */
 
         return rootView;
     }
