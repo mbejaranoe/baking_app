408572a922abaf4990d8d1ad79d6d97823e5bd1e
Manolo <mbejaranoe@gmail.com>
2017-11-15 11:50:38 +0100

    Implement RecipeWidget's onUpdate method

--- app/src/main/java/com/example/android/mbejaranoe/bakingapp/RecipeWidget.java
+++ app/src/main/java/com/example/android/mbejaranoe/bakingapp/RecipeWidget.java
@@ -7,6 +7,7 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Build;
 import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
@@ -21,7 +22,7 @@ public class RecipeWidget extends AppWidgetProvider {
 
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
-
+        /*
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.recipe_widget);
 
@@ -46,14 +47,48 @@ public class RecipeWidget extends AppWidgetProvider {
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
+        */
     }
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         // There may be multiple widgets active, so update all of them
         for (int appWidgetId : appWidgetIds) {
-            updateAppWidget(context, appWidgetManager, appWidgetId);
+
+            // Set up the intent that starts the RecipeWidgetService, which will
+            // provide the views for this collection.
+            Intent intent = new Intent(context, RecipeWidgetService.class);
+            // Add the app widget ID to the intent extras.
+            intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
+            intent.setData(Uri.parse(intent.toUri(Intent.URI_INTENT_SCHEME)));
+            // Instantiate the RemoteViews object for the app widget layout.
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.recipe_widget);
+            // Set up the RemoteViews object to use a RemoteViews adapter.
+            // This adapter connects
+            // to a RemoteViewsService  through the specified intent.
+            // This is how you populate the data.
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
+                setRemoteAdapter(context, views);
+            } else {
+                setRemoteAdapterV11(context, views);
+            }
+
+            // Set the text for the widget header
+            SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+            String recipeName = sharedPreferences.getString("name", "none");
+            int recipe_Id = sharedPreferences.getInt("recipe_Id", -1);
+            Log.v(RecipeWidget.class.getSimpleName(), "recipeName: " + recipeName);
+            if (recipeName.equals("none") || recipe_Id == -1) {
+                views.setTextViewText(R.id.widget_header_text_view,
+                        context.getResources().getString(R.string.widget_message_no_recipe_selected));
+            } else {
+                views.setTextViewText(R.id.widget_header_text_view, recipeName);
+            }
+
+            // Instruct the widget manager to update the widget
+            appWidgetManager.updateAppWidget(appWidgetId, views);
         }
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
     }
 
     @Override
